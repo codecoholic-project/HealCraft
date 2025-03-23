c89783972f91fcb1b4920a755f4eaa6aab98ce71
prabi <prabi@Prabir-Pc>
2025-03-23 18:25:22 +0530

    initial commit

---
+++ HealCraft/src/main/java/com/web/HealCraft/HealCraftApplication.java
@@ -0,0 +1,13 @@
+package com.web.HealCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealCraftApplication.class, args);
+	}
+
+}
---
+++ HealCraft/src/test/java/com/web/HealCraft/HealCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.HealCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
